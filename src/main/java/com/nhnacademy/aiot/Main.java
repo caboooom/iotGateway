bbdb899e2c0dd1e3a161bb113a1fef4be1b455f9
ckddms6530 <ckddms6530@gmail.com>
2023-12-02 17:07:14 +0900

    Filter, Split  구현 완료 및 Main 수정

--- src/main/java/com/nhnacademy/aiot/Main.java
+++ src/main/java/com/nhnacademy/aiot/Main.java
@@ -4,8 +4,6 @@ import com.nhnacademy.aiot.node.DebugNode;
 import com.nhnacademy.aiot.node.FilterNode;
 import com.nhnacademy.aiot.node.MqttInNode;
 import com.nhnacademy.aiot.node.MqttOutNode;
-import com.nhnacademy.aiot.node.Node;
-import com.nhnacademy.aiot.node.SensorTypeFilterNode;
 import com.nhnacademy.aiot.node.SplitNode;
 import com.nhnacademy.aiot.util.Config;
 
@@ -18,47 +16,39 @@ public class Main {
         command.set();
         MqttInNode mqttInNode = new MqttInNode(1, "tcp://ems.nhnacademy.com", "cla");
         MqttOutNode mqttOutNode = new MqttOutNode(1, "tcp://localhost:1883");
-        SensorTypeFilterNode filterNode = new SensorTypeFilterNode(1, 1);
-        Node debugNode = new DebugNode();
-        
+
+        DebugNode spliterDebugNode = new DebugNode();
         DebugNode filterDebuger = new DebugNode();
 
-        FilterNode filterNode2 = new FilterNode(1, 1 , new String[]{"place", "devEui", "object","branch"});
+        FilterNode filterNode = new FilterNode(1, 1 , new String[]{"place", "devEui", "object","branch"});
         SplitNode splitNode = new SplitNode(1, 1, "object");
 
         Wire wire = new Wire();
+        Wire wire1 = new Wire();
         Wire wire2 = new Wire();
+        Wire wire3 = new Wire();
+        Wire wire4 = new Wire();
       
         mqttInNode.setOutputWire(0, wire);
-        
         filterNode.setInputWire(0, wire);
 
-        //split Node
-        Wire wire4 = new Wire();
+        filterNode.setOutputWire(0, wire2);
+        filterDebuger.setInputWire(0, wire2);
 
-        //filter Wire;
-        Wire wire5 = new Wire();
-        Wire wire6 = new Wire();
+        filterNode.setOutputWire(0, wire1);
+        splitNode.setInputWire(0, wire1);
 
-        //filterToSplitWire
-        mqttInNode.setOutputWire(0, wire5);
-        filterNode2.setInputWire(0, wire5);
-        filterNode2.setOutputWire(0, wire6);
-        filterNode2.setOutputWire(0, wire);
-        filterDebuger.setInputWire(0, wire6);
-
-        splitNode.setInputWire(0, wire);
+        splitNode.setOutputWire(0, wire3);
+        spliterDebugNode.setInputWire(0, wire3);
+        
         splitNode.setOutputWire(0, wire4);
-        debugNode.setInputWire(0,  wire4);
-        //debugNode.setInputWire(0,  wire1);
-
-        mqttOutNode.setInputWire(0, wire2);
+        mqttOutNode.setInputWire(0, wire4);
 
         mqttInNode.start();
-        mqttOutNode.start();
-        debugNode.start();
+        //mqttOutNode.start();
+        filterNode.start();
         splitNode.start();
-        filterNode2.start();
+        spliterDebugNode.start();
         filterDebuger.start();
     }
 }
